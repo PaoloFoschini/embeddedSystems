92a6d2a90b1c94fd01d9f7ca2462c8928f09bf04
Paolo Foschini - paolo.foschini2@studio.unibo.it <paolo.foschini2@studio.unibo.it>
2025-10-01 11:32:54 +0200

    Second Lesson

--- buttonLed/buttonLed.ino
+++ buttonLed/buttonLed.ino
@@ -1,19 +1,33 @@
 #define LED 12
 #define BUTTON 2
 
+bool ledAlreadyOn;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT);
+  ledAlreadyOn = false;
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
   /*if(digitalRead(BUTTON) == HIGH){
     digitalWrite(LED, HIGH);
   }
   digitalWrite(LED, LOW);
   */
-  digitalWrite(LED, digitalRead(BUTTON));
+  //digitalWrite(LED, digitalRead(BUTTON));
+  int buttonState = digitalRead(BUTTON);
+  if(buttonState == HIGH && !ledAlreadyOn){
+    Serial.println("PRESSED");
+    digitalWrite(LED, HIGH);
+    ledAlreadyOn = true;
+  } else if(buttonState == LOW && ledAlreadyOn){
+    Serial.println("RELEASED");
+    digitalWrite(LED, LOW);
+    ledAlreadyOn = false;
+  }
 }
